4470610f36a26715fc3db78bf327c994e5b4fa9a
Ashish Shukla <122502973+ashish8112@users.noreply.github.com>
2025-08-29 14:57:51 +0530

    Create first_swing.java

---
+++ College_Lab/first_swing.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.event.*;
+class Demo{
+public static void main(String args[])
+{
+JFrame frame= new JFrame("Swing Example");
+JButton button = new JButton("Click Me");
+
+button.addActionListener(new ActionListener(){
+public void actionPerformed (ActionEvent e){
+JOptionPane.show.MessageDialog(frame,"Button Clicked!");
+}
+}
+frame.add(button);
+frame.setSize(300,200);
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+frame.setVisible(true);
+}
+}
