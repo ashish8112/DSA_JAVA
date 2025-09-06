72b253e3198f04661533f5b750ad463216b56cab
ashish8112 <ashish233227shukla@gmail.com>
2025-09-06 14:02:16 +0530

    Palindrome Number

--- Pattern_Star/Pattern02.java
+++ Pattern_Star/Pattern02.java
@@ -6,7 +6,7 @@ public class Pattern02 {
             for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 }
