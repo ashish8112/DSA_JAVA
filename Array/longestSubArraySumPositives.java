20ec00c42e2734826dceb4aba338948f75e399c1
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-01-09 00:08:45 +0530

    Create longestSubArraySumPositives.java

---
+++ Array/longestSubArraySumPositives.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class longestSubArraySumPositives {
+    static int longestSubArraySum(int [] arr,int target)
+    {
+        int sum=0;
+        int j=0;
+        int maxLength=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            sum+=arr[i];
+            if(sum==target)
+            {
+                maxLength=Math.max(maxLength,i-j+1);
+            }
+            while(j<=i&&sum>target)
+            {
+                sum-=arr[j++];
+                if(sum==target)
+                {
+                    maxLength=Math.max(maxLength,i-j+1);
+                }
+            }
+        }
+        return maxLength;
+    }
+    public static void main(String[] args) {
+     System.out.println("Enter the size of the array ");
+     Scanner sc= new Scanner(System.in);
+     int size=sc.nextInt();
+     int [] arr= new int [size];
+     System.out.println("Enter the values in the array ");
+     for(int i=0;i<arr.length;i++)
+     arr[i]=sc.nextInt();
+     System.out.print("Enter the target for the longest subaaray ");
+     int target=sc.nextInt();
+     int length=longestSubArraySum(arr,target);
+     System.out.println("Length of target " + target + " is " + length);
+    }
+}
