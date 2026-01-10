5f895cd75fe96aa78736ca70d7dc2f5670a59784
ashish8112 <ashish233227shukla@gmail.com>
2026-01-10 14:43:40 +0530

    Array

--- Array/Easy/FindNumberAppearsOnce.java
+++ Array/Easy/FindNumberAppearsOnce.java
@@ -1,6 +1,23 @@
 package DSA.Array.Easy;
 
+import java.util.Scanner;
+
 public class FindNumberAppearsOnce {
+    public static void findNumberAppearsOnce(int [] arr)
+    {
+        int xor=0;
+        for(int i=0;i<arr.length;i++)
+            xor=xor^arr[i];
+        System.out.println(xor+" This number Appears once");
+    }
     public static void main(String[] args) {
+        System.out.println("Enter the size of the array ");
+        Scanner sc= new Scanner(System.in);
+        int size=sc.nextInt();
+        int [] arr= new int [size];
+        System.out.println("Enter the values in the array ");
+        for(int i=0;i<arr.length;i++)
+            arr[i]=sc.nextInt();
+        findNumberAppearsOnce(arr);
     }
 }
