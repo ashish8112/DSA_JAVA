bc2282ff68ff48362cb6c320055a3632bad83ce0
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-01-23 09:55:31 +0530

    Update MaximumSubarray question
    Using Kadane's Algorithm

--- Array/Medium/05_MaximumSubarray.java
+++ Array/Medium/05_MaximumSubarray.java
@@ -29,6 +29,19 @@ class MaximumSubarray{
       }
     return max;
   }
+  public static int maximumSubarrayOpt(int [] arr) // Kadane's Algorithm -> if sum <0 reset sum to 0 becuase we don't need to add negative numbers only one negative number can be largest sum.
+  {
+    int max=Integer.MIN_VALUE;
+    int sum =0
+    for(int i=0;i<arr.length;i++)
+      {
+        sum+=arr[i];
+        max=Math.max(sum,max);
+        if(sum<0)
+          sum=0;
+      }
+    return max;
+  }
   public static void main(String [] args)
   {
     System.out.println("Enter the size of the array ");
@@ -38,7 +51,7 @@ class MaximumSubarray{
     int [] nums = new int [size];
     for(int i=0;i<size;i++)
       nums[i]=sc.nextInt();
-    int max = maximumSubarray(nums);
+    int max = maximumSubarrayOpt(nums);
     System.out.println("The maximum sum of Array is "+max);
   }
 }
