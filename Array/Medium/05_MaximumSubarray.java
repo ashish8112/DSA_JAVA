7d0cfaa97c97cd947b317cff6f084aae8df5e9cd
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-01-23 10:10:05 +0530

    Update 05_MaximumSubarray.java
    Added printing of Subarray with maximum sum

--- Array/Medium/05_MaximumSubarray.java
+++ Array/Medium/05_MaximumSubarray.java
@@ -42,6 +42,30 @@ class MaximumSubarray{
       }
     return max;
   }
+
+  public static int printMaximumSubarrayOpt(int [] arr)//Print MaximumSubarray
+  {
+    int max=Integer.MIN_VALUE;
+    int sum =0;
+    int start=0;
+    int end=0;
+    for(int i=0;i<arr.length;i++)
+      {
+          if(sum<0)
+          start=i;
+        sum+=arr[i];
+        if(sum>max)
+        {
+            max=sum;
+            end=i;
+        }
+        if(sum<0)
+          sum=0;
+      }
+      for(int i=start;i<=end;i++)
+      System.out.print(arr[i]+" ");
+    return max;
+  }
   public static void main(String [] args)
   {
     System.out.println("Enter the size of the array ");
