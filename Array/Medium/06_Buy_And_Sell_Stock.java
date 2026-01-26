9d07ab48b8777965527156365defa034b836ab83
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-01-27 00:31:07 +0530

    Update 06_Buy_And_Sell_Stock.java
    Added Optimal Approach

--- Array/Medium/06_Buy_And_Sell_Stock.java
+++ Array/Medium/06_Buy_And_Sell_Stock.java
@@ -1,6 +1,6 @@
 import java.util.*;
 class Buy_And_Sell_Stock{
-  public int maxiProfitBrute(int [] arr)
+  public int maxiProfitBrute(int [] arr) //Brute Approach
   {
     int min=Integer.MAX_VALUE;
     int profit=0;
@@ -16,8 +16,21 @@ class Buy_And_Sell_Stock{
                 profit=arr[j]-arr[i];
                 min=arr[i];
               }
+              j++;
             }
         }
+      }
+    return profit;
+  }
+  public int maxiProfitOpt(int [] arr) // Optimal Approach
+  {
+    int min=arr[0];
+    int profit=0;
+    for(int i=1;i<arr.length;i++)
+      {
+        int cost = arr[i]-min;
+        profit=Math.max(cost,profit);
+        min=Math.min(min,arr[i]);
       }
     return profit;
   }
