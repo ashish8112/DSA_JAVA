d62f258a5c8766e9f4ff8ef1e477cd303f6f461e
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-01-29 23:12:58 +0530

    Update 07_RearrangeArrayElementsBySign.java
    Added Optimal Approach

--- Array/Medium/07_RearrangeArrayElementsBySign.java
+++ Array/Medium/07_RearrangeArrayElementsBySign.java
@@ -20,6 +20,24 @@ class RearrangeArrayElementsBySign{
         }
         return nums;
     }
+   public int[] rearrangeArrayOpt(int[] nums) {
+       int [] arr = new int [nums.length];
+       int j=0,k=0;
+       for(int i=0;i<nums.length;i++)
+       {
+        if(nums[i]<0)
+        {
+            arr[j*2+1]=nums[i];
+            j++;
+        }
+        else if(nums[i]>-1)
+        {
+            arr[k*2]=nums[i];
+            k++;
+        }
+       }
+        return arr;
+    }
   public static void main(String [] args)
   {
     Scanner sc = new Scanner(System.in);
@@ -29,7 +47,7 @@ class RearrangeArrayElementsBySign{
     int [] arr = new int [size];
     for(int i=0;i<arr.length;i++)
       arr[i]=sc.nextInt();
-    arr=rearrangeArray(arr);
+    arr=rearrangeArrayOpt(arr);
     for(int i=0;i<arr.length;i++)
       System.out.println(arr[i]+" ");
   }
