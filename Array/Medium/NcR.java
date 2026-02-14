dee414169f8245efe01152ecacbd841cda0ef64c
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-02-14 14:06:54 +0530

    Update NcR.java
    Added Optimal approach of NcR

--- Array/Medium/NcR.java
+++ Array/Medium/NcR.java
@@ -18,6 +18,16 @@ class NcR{
         int value = factr/(fr*factc);
         System.out.println("Value of Nth col of Nth row is "+value);
   }
+     public static void impr(int row,int col) // Optimal Approach for finding NcR
+  {
+      int value =1;
+      for(int i=0;i<col;i++)
+      {
+        value = value*(row--);//ex-> 5c2 = (5 * 4)Numerator && (2 * 1)Denominator so 5/2 will cause inconsisent in Integer(int) so 5/1 -> 4/2  5*2=10.
+        value = value/(i+1);
+      }
+      System.out.println("Value of Nth col of Nth row is "+value);
+  }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the row ");
