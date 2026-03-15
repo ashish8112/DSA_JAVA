c7667ef0b2be10f86047bf50939bce5f33931ac3
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-03-15 17:50:55 +0530

    Add MaxProductSubarray class with main method
    Implement a method to find the maximum product of a subarray and include user input functionality.

---
+++ Array/Hard/03_MaximumProductSubarray.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class MaxProductSubarray {
+
+    public static int maxProduct(int[] nums) {
+        int pre = 1, suf = 1;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            pre *= nums[i];
+            suf *= nums[nums.length - i - 1];
+            max = Math.max(max, Math.max(pre, suf));
+            if (pre == 0) pre = 1;
+            if (suf == 0) suf = 1;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println("Array: " + Arrays.toString(nums));
+        System.out.println("Max Product: " + maxProduct(nums));
+    }
+}
