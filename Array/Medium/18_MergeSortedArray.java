0035806b7b977c917241466eeab0ce43b60736e0
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-03-24 16:23:55 +0530

    Add alternate merge methods for sorted arrays
    Added two new methods for merging arrays: mergeAlternate1 and mergeAlternate2. These methods provide alternate approaches to merging two sorted arrays into one.

--- Array/Medium/18_MergeSortedArray.java
+++ Array/Medium/18_MergeSortedArray.java
@@ -17,6 +17,46 @@ class MergeSortedArray{
         }
       }
   }
+  public static void mergeAlternate1(int[] nums1, int m, int[] nums2, int n) {
+        int i=m-1;
+        int j=n-1;
+        int k=nums1.length-1;
+        while(j>=0&&i>=0)
+        {
+            if(nums1[i]>=nums2[j])
+            {
+                nums1[k--]=nums1[i--];
+            }
+            else{
+                nums1[k--]=nums2[j--];
+            }
+        }
+        while(j>=0)nums1[k--]=nums2[j--];//because I exhausted every element in I are added in nums1 because they are larger no need to check i>=0 and append
+    }
+  public static void mergeAtlernate2(int[] nums1, int m, int[] nums2, int n) {  
+        int i=m-1;
+        int j=n-1;
+        int k=nums1.length-1;
+        while(i>=0&&j>=0)
+        {
+            if(nums1[i]>=nums2[j])
+            {
+                nums1[k--]=nums1[i--];
+            }
+            else 
+            {
+                nums1[k--]=nums2[j--];
+            }
+        }
+        while(i>=0)  //Thinking like merging two arrays in another array although it doesn't require reason  is written above 
+        {
+            nums1[k--]=nums1[i--];
+        }
+        while(j>=0)
+        {
+            nums1[k--]=nums2[j--];
+        }
+    }
   public static void main(String [] args)
   {
         Scanner sc = new Scanner(System.in);
