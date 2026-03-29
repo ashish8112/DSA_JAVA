f4f7a8c2b9c453a66fdd91eaa14fe33a35c50797
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-03-29 19:37:53 +0530

    Add NullPointer handling examples in NullPointerSolving.java
    Demonstrate handling NullPointerExceptions with Maps in Java.

---
+++ MAP/NullPointerSolving.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class NullPointer{
+  public static void main(String [] args)
+  {
+    Map<String, Integer> map = new HashMap<>();
+    map.put("a", 5);
+
+    //Problem 1 -> int val = map.get("b");  //  NPE!
+    // "b" exist hi nahi — get() returns NULL
+    // null ko int mein unbox karna — BOOM!
+
+    // Option 1 — getOrDefault
+    int val = map.getOrDefault("b", 0);  //  "b" nahi mila toh 0 return 
+
+    // Option 2 — containsKey check
+    if (map.containsKey("b")) {
+    int val2 = map.get("b");
+    }
+    
+    // Problem 2 Classic frequency problem
+    Map<Character,Integer>map2=new HashMap<>();
+    String s= "Ashish";
+    for (char c : s.toCharArray()) {
+   // map2.put(c, map2.get(c) + 1);  //  NPE — pehli baar c nahi hai map mein , map.get(c) = null — null + 1 = BOOM
+      // getOrDefault se
+    map2.put(c, map2.getOrDefault(c, 0) + 1);  //  pehli baar 0+1 = 1
+      }
+
+    //Problem 3 
+    Map<Integer, Integer> map3 = new HashMap<>();
+    map3.put(1,10);
+    //int val = map3.get(key);           //  NPE if key missing — null → int unbox
+    Integer val3 = map.get(2);       //  null store ho jaayega — NPE nahi
+  }
+}
