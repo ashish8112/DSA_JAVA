a9eb24dd7162872c58df57fc7f6a35fc6d8c1d01
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-04-15 16:14:12 +0530

    Add solution for finding kth missing positive integer
    Implement a solution to find the kth missing positive integer.

---
+++ BinarySearch/20_KthMissingPositiveInteger.java
@@ -0,0 +1,24 @@
+//Brute
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        HashMap<Integer,Integer>present = new HashMap<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            present.put(arr[i],i);
+        }
+        int j = 1;
+        for(int i=1;i<=arr.length+k;i++)
+        {
+            if(!present.containsKey(i))
+            {
+                if(j==k)
+                return i;
+                j++;
+            }
+        }
+        return 0;
+    }
+}
+
+time complexity = O(n)+O(n+k) = O(n)
+space complexity = O(n) 
