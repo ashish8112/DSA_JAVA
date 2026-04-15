3cb3dc61785e2a91e1e2f293113cec8f52347a46
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-04-15 15:26:11 +0530

    Refactor shipWithinDays method for clarity and efficiency

--- BinarySearch/19_CapacityToShipPackages.java
+++ BinarySearch/19_CapacityToShipPackages.java
@@ -1,46 +1,40 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int high = 0;//will be enitre sum of weights
-        int low = weights[0];//will be maximum weight of package
-        int ans =-1;
+        int max = weights[0];
+        int sumOfWeights = 0;
+        int ans =-1; 
         for(int i=0;i<weights.length;i++)
         {
-        high+=weights[i];
-        low=Math.max(low,weights[i]);
+            max=Math.max(max,weights[i]);
+            sumOfWeights+=weights[i];
         }
+        int low = max;
+        int high = sumOfWeights;
         while(low<=high)
         {
-            int mid = low+(high-low)/2;
-            int sum =0;
-            int cap =0;
+            int capacityOfShip = low+(high-low)/2;
+            long sum =0;
+            int shippingDay =0;
             for(int i=0;i<weights.length;i++)
             {
-                if(sum+weights[i]<=mid)
+                if(sum+weights[i]<=capacityOfShip)
                 sum+=weights[i];
                 else{
-                    cap++;
-                    sum=weights[i];
+                    shippingDay++;
+                    sum = weights[i];
                 }
             }
-            if(sum<=mid)
-            cap++;
-            if(cap<=days)
+            if(sum<=capacityOfShip)
+            shippingDay++;
+            if(shippingDay<=days)
             {
-                ans = mid;
-                high = mid-1;
+                high = capacityOfShip -1;
+                ans = capacityOfShip;
             }
             else{
-                low=mid+1;
+                low = capacityOfShip + 1;
             }
         }
         return ans;
     }
-
-  /*Pattern: Binary Search on Answer
-Trigger: "minimum capacity/speed/divisor such that condition satisfies within limit"
-Template: low=max(weights), high=sum(weights), check(mid) → count ships <= days
-Key Insights: low = max(weights) — physically impossible below this
-              high = sum(weights) — worst case, 1 package per day
-              after loop cap++ — last ship count karna mat bhoolo
-Complexity: Time O(n log(sum-max)), Space O(1)*/
 }
