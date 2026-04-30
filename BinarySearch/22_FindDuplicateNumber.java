4c264cb53f48f91f84e214eba735aad5af4c62bc
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-04-30 19:41:05 +0530

    Clarify complexity constraints in findDuplicate method
    Updated comments to clarify space and time complexity of the findDuplicate method.

--- BinarySearch/22_FindDuplicateNumber.java
+++ BinarySearch/22_FindDuplicateNumber.java
@@ -1,9 +1,9 @@
-class Solution { //But it violates the constraint of o(1) space complexity
+class Solution { //But it violates the constraint of o(1) space complexity but time complexity is o(n) 
     public int findDuplicate(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
         for(int i=0;i<nums.length;i++)
         {
-        if(!set.contains(nums[i]))
+        if(!set.contains(nums[i]))    // time complexity o(1) because back in hood it uses hashmap
         set.add(nums[i]);
         else
         return nums[i];
