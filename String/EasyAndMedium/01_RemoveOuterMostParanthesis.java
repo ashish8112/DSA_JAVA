966432163aa62e82aa5f323debc8dc6ce7d9cedf
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-05-01 20:44:21 +0530

    Refactor RemoveOuterParentheses to Solution class

--- String/EasyAndMedium/01_RemoveOuterMostParanthesis.java
+++ String/EasyAndMedium/01_RemoveOuterMostParanthesis.java
@@ -1,30 +1,21 @@
-import java.util.*;
-
-class RemoveOuterParentheses {
-
-    public static String removeOuter(String s) {
-        StringBuilder out = new StringBuilder();
-        int count = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                if (count > 0) out.append('(');
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder res = new StringBuilder();
+        int count =0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(')
+            {
                 count++;
-            } else {
+                if(count>=2)
+                res.append("(");
+            }
+            else{
                 count--;
-                if (count > 0) out.append(')');
+                if(count>=1)
+                res.append(")");
             }
         }
-
-        return out.toString();
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the parentheses string:");
-        String s = sc.next();
-
-        System.out.println("Input  : \"" + s + "\"");  // \" used to display " in java 
-        System.out.println("Output : \"" + removeOuter(s) + "\"");
+        return res.toString();
     }
 }
