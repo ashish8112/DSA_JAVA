e59088d6435b4dbfdcdf28ddac96d77a1aae0950
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-05-06 20:14:08 +0530

    Update 22_FindDuplicateNumber.java

--- BinarySearch/22_FindDuplicateNumber.java
+++ BinarySearch/22_FindDuplicateNumber.java
@@ -1,4 +1,4 @@
-class Solution { //But it violates the constraint of o(1) space complexity but time complexity is o(n) 
+class Solution { //But it violates the constraint of o(1) space complexity but time complexity is o(n).
     public int findDuplicate(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
         for(int i=0;i<nums.length;i++)
