1fb284dc78976611f264022667310d5e2e941b45
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-05-25 20:06:03 +0530

    Refactor shipping day logic and enhance comments
    Refactor shipping day calculation logic and improve comments for clarity.

--- BinarySearch/19_CapacityToShipPackages.java
+++ BinarySearch/19_CapacityToShipPackages.java
@@ -23,8 +23,8 @@ class Solution {
                     shippingDay++;
                     sum = weights[i];
                 }
-            }
-            if(sum<=capacityOfShip)
+            
+            if(sum<=capacityOfShip)    // or directly shippingDay++ because if condition will always be true no matter what . Reasons is written below.
             shippingDay++;
             if(shippingDay<=days)
             {
@@ -38,3 +38,14 @@ class Solution {
         return ans;
     }
 }
+//pattern : Binary Search on answers
+//approach : we should start with maximum weight because ship must load every weight if shippment weight is smaller than package weight and than one enitre ship 
+// will not also able to carry that package so that is invalid weight of ship so start with maximum of array (maximum weight of package) and hgih must be some
+// of all weights so it can shipped in most least number of days which is practically 1 , so entire package  can be shipped in 1 day . 
+
+//Problem why at last (sum<=capacityOfShip) because it starts with maximum of array (value) so even the last value is exist in weight it will come under the 
+// capacityOfShip so +1  only and if last value exceed the capacity inner loop else will execute and add one more day of shippment and remaining weight will surely
+// come under capacityOfShip because a single last value can be maximum of which is lowest of our range so it is valid to increase one day 
+// and another case if all weights come inside the capacityofship and else part of loop doesn't execute so after loop shippingDay ++ is necessary and valid 
+// so conclusion is this we have to increase one day ++ after the loop and no need to check sum <=capacityOfShip but for more readibility and understanding 
+// I have written that if condition will always be true no matter what
