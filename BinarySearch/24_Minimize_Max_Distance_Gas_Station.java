8d23824126fcad1f2d0110fde09df92476abbbf5
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-06-02 12:53:36 +0530

    Refine comments for gas station distance minimization
    Updated comments to clarify the binary search approach for minimizing maximum distance between gas stations.

--- BinarySearch/24_Minimize_Max_Distance_Gas_Station.java
+++ BinarySearch/24_Minimize_Max_Distance_Gas_Station.java
@@ -1,8 +1,10 @@
+//Pattern - Binary Search on Answer 
+// Approach - low = 1 and high = maximum gap between any two consectuive gas station 
 // ex -> arr = [1,7] , k=2
 // My observation is take any distance starts from 1 because the smallest distance between any two gas station is 1 so 
 // we will try to set add gas station between two consecutive distance of gas station for that we will check 
 // d(distance yet = 1) no of possible gas station between two distance = distance between two station / d (1) - 1 . ? why 
-// -1 because it will divide that space between equal parts suppose arr = [1, 7] => 6/2 which is 3 so there are will be three space or three sector 1__x__x__7 so 
+// -1 because it will divide that space between equal parts suppose arr = [1, 7] => (ceiling) 6/2 which is 3 so there are will be three space or three sector 1__x__x__7 so 
 // there is three equal space of 2 which was d or distance so how many station can be fixed which 2 so count will be 2 1 <-2-> A <-2-> B <-2-> 7  gas station A and B 
 //now come to the question we will start from 1 distance so 6/1-1 = 5 gas station required to get minimum distance 1 but question is given k=2 , so this is not possible 
 // so we will take d = 2 so get 6/2 -1 = 2 so this possible answer because k is 2 and we get 2 gas station which help to minimize the distance between 2 gas station 
@@ -13,4 +15,11 @@
 
 // let see any another example 
 // ex -> arr = [3, 6, 12, 19, 33], k = 3
-// 
+// so all distance 3 <3> 6 <6> 12 <7> 19 <14> 33   , so there are 4 sector of space V X Y Z 
+// let's start with d = 1 in first two consecutive distance there would be 2 station and in second consecutive 6/1 - 1 = 5 and so on but k is only 3 so it cannot minimize distance to 1 using k = 3 
+// let's take d = 2 then V space = 3 / 2 = > 1.X take celling 2 => 2-1 we can fix 1 station here, X space = 6/2 = 3-1 = 2 and so on total 12 station required to get minimum distance as 2 
+// let's take d = 5 then V space = 3/5 => 0.X => 1-1= 0 , X space = 6/5 = > 2-1 = 1 station , Y space = 7/5 => 2-1 = 1 station , Z space = 14 /5 => 3 -1 = 2 so total station required 4 but given is 3 this is also not valid
+// let's take d = 6 then V space = 3/6 => 1-1= 0 , X space = 6/6 -1 = 0 , Y space = 7/6 => 2-1 = 1 station  and Z = 14/6 => 3-1 = 2 station so total is 3 station is required and possible minimum maximum distance with 
+// 3 station is 6 so answer is 6. even though all distance above 6 distance like 7,8,9 and 14 is valid because it will take less than or equall to k station to minimize the distance but our aim is to find 
+// minimum distance which has maximum distance between two station so taking 6 will be like that -> 3 6 12 <6> A(18) <1> 19 <6> B(25) <6> C(31) <2> 33  so tthree station A, B and C is used 
+// we can notice that 6 is minimum maximum distance in between the gas station.
