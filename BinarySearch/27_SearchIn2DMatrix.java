3d0f771cb733398281221f32f80bb20ec2013bf9
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-06-07 21:04:02 +0530

    Improve comments for index calculations in 2D matrix
    Clarified comments explaining index calculations for 2D matrix search.

--- BinarySearch/27_SearchIn2DMatrix.java
+++ BinarySearch/27_SearchIn2DMatrix.java
@@ -65,8 +65,8 @@ Virtual index  0  1  2  3  4   5   6   7   8   9  10   11
 // because as we know in 1d array also array.length is entire length 12 if it is 1d but last index is 12-1 
     // and now we can calculate the correct index using mid if low =0 and high =11 
 // suppose we are creating circular queque so what we index+1%arr.length to get 0 th location if we are at end 
-// that's how there are 4 column in each row so we can get col index using mid%col for example 7th virtual index but original index is 13 
-    // if we take remainder of 7%4 we get 4 = 3 and now how row .. think like we are adding 4 element's array together and there are 4 array in a line 
+// that's how there are 4 column in each row so we can get col index using mid%col for example 7th virtual index (20) but original index is [1][3]  if
+// we take remainder of 7%4 we get 4 = 3 which is col index and now how row .. think like we are adding 4 element's array together and there are 4 array in a line 
     // if 4 element complete means 0th array is completed andn  1st array is started that how we get row index  *virtualIndex/col* why col because 
     // each array is consist of col number of value in a each array so if number entirely divided we get the position of that index , that where it is 
     // stored in 0th array , 1st array or so on 
