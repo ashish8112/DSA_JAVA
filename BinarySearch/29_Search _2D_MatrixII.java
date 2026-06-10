7392bacb6a8e3cd2fc58bb3e75439a8d7a9de6e8
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-06-10 10:34:55 +0530

    Add optimal searchMatrix method for 2D matrix
    Implemented an optimal search algorithm for a 2D matrix using a staircase search approach.

--- BinarySearch/29_Search _2D_MatrixII.java
+++ BinarySearch/29_Search _2D_MatrixII.java
@@ -30,3 +30,35 @@ class Solution { // Better Approach
         return false;
     }
 }
+
+class Solution { // Optimal Approach 
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i=0;
+        int j=matrix[0].length-1;
+        while(i<matrix.length&&j>=0)
+        {
+            if(matrix[i][j]==target)
+            return true;
+            else if(matrix[i][j]<target)
+            i++;
+            else
+            j--;
+        }
+        return false;
+    }
+}
+
+// Pattern : Staircase Search — 2D Matrix
+// what is question each row is sorted in ascending order but columns are sorted in ascending order too 
+// that means last value of 1st row is not may be smaller then 1st value of second row because see question 
+// Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+        1    4    7    11    15  <- it is sorted in ascending order 
+        2    5    8    12    19  and columns are also sorted in ascending 
+        3    6    9    16    22
+        10   13   14   17    24
+        18   21   23   26    30
+// core idea is to start form top right corner as it is largest in first row , if currentIndex value is the target value return true or if current value is larger than target
+// then move pointer from currentIndex to one previous one why because all elements in that column will be larger than current value so move left 
+// and if currentIndex value is smaller than target then move pointer to next row from 0th row to 1st row because element is greater than that in next row column , now we can think
+// why next row , why not current row and move right side as greater element is also in right side of that current index because we are here after checking that row as we are coming 
+// form top right corner so we moved left because those values were greater than target value 
