9ccb256d55343753f93c332fc3da4247a15c6b96
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-06-10 10:23:09 +0530

    Add binary search implementation for 2D matrix
    Implemented a more efficient search algorithm for a 2D matrix using binary search.

--- BinarySearch/29_Search _2D_MatrixII.java
+++ BinarySearch/29_Search _2D_MatrixII.java
@@ -9,3 +9,24 @@ class Solution { // Brute Force
         return false;
     }
 }
+
+class Solution { // Better Approach
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for(int i=0;i<matrix.length;i++)
+        {
+            int low=0;
+            int high = matrix[i].length-1;
+            while(low<=high)
+            {
+                int mid = low+(high-low)/2;
+                if(matrix[i][mid]==target)
+                return true;
+                else if (matrix[i][mid]<target)
+                low=mid+1;
+                else 
+                high = mid -1;
+            }
+        }
+        return false;
+    }
+}
