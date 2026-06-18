c380bbfd58e862c04365dfb640f4ec7d4c1873a0
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-06-18 22:51:39 +0530

    Implement findPeakGrid method for peak finding

--- BinarySearch/30_FindAPeakElementII.java
+++ BinarySearch/30_FindAPeakElementII.java
@@ -19,3 +19,32 @@ class Solution { //Brute Force
          return new int [] {-1,-1};
     }
 }
+
+class Solution { //Optimal
+    public int[] findPeakGrid(int[][] matrix) {
+    int low =0;
+    int high = matrix[0].length-1;
+    int row =0;
+    while(low<=high)
+    {
+        int col = low+(high-low)/2;
+        for(int i=0;i<matrix.length;i++)
+        {
+            if(matrix[row][col]<matrix[i][col])
+            row=i;
+        }
+        if(col<matrix[0].length-1&&matrix[row][col]<matrix[row][col+1])
+        {
+            low=col+1;
+            continue;
+        }
+        if(col>0&&matrix[row][col]<matrix[row][col-1])
+        {
+            high = col-1;
+            continue;
+        }
+        return new int [] {row,col};
+    }
+        return new int [] {-1,-1};
+    }
+}
