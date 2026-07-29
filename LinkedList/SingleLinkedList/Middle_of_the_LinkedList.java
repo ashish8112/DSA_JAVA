7fd5b696f92ee965b09a39cad13001c89e48aa69
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-07-29 22:09:30 +0530

    Add Middle_of_the_LinkedList.java solution
    Implement a solution to find the middle node of a singly linked list.

---
+++ LinkedList/SingleLinkedList/Middle_of_the_LinkedList.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        int count =0;
+        ListNode traverse = head;
+        while(traverse!=null)
+        {
+            count++;
+            traverse=traverse.next;
+        }
+        count/=2; // why not +1 because our head is at first location always we need only half of number of it is odd because of int we get min value and if it is even there are two middle and question wants from second so it also perfect there.
+        traverse=head;
+        int check =1; 
+        while(check<=count)
+        {
+            traverse=traverse.next;
+            check++;
+        }
+        return traverse;
+    }
+}
