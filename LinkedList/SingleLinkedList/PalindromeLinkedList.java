58d4e0f44c091e50c095fa8914832060c2040641
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-08-03 23:07:22 +0530

    Implement palindrome check for linked lists
    Added two approaches to check if a linked list is a palindrome. The first modifies the list, while the second preserves the original list structure.

--- LinkedList/SingleLinkedList/PalindromeLinkedList.java
+++ LinkedList/SingleLinkedList/PalindromeLinkedList.java
@@ -42,3 +42,83 @@ class Solution { //Brute using List
         return true;
     }
 }
+
+class Solution { //Optimal Approach but linkedlist is modified not same as actuall 
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null&&fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode prev = slow;
+        slow=slow.next;
+        prev.next=null;
+        ListNode curr = slow;
+        while(slow!=null)
+        {
+            slow=slow.next;
+            curr.next = prev;
+            prev=curr;
+            curr=slow;
+        }
+        curr=prev;
+        slow=head;
+        while(curr!=null)
+        {
+            if(slow.val==curr.val)
+            {
+                slow=slow.next;
+                curr=curr.next;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
+//Approach reverse the linked List from the middle means point middle to null and last element from point null to its previouus element 
+
+class Solution { //Same as above but linkedlist remaine same as before after palindrome checks
+    public static ListNode reverse(ListNode add)
+    {
+        ListNode prev = null;
+        ListNode curr = add;
+        while(add!=null)
+        {
+            add=add.next;
+            curr.next=prev;
+            prev=curr;
+            curr=add;
+        }
+        return prev;
+    }
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null&&fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode ogTail = reverse(slow);
+        ListNode curr=ogTail;
+        slow=head;
+        while(curr!=null)
+        {
+            if(slow.val==curr.val)
+            {
+                slow=slow.next;
+                curr=curr.next;
+            }
+            else{
+                reverse(ogTail);
+                return false;
+            }
+        }
+        reverse(ogTail);
+        return true;
+    }
+}
