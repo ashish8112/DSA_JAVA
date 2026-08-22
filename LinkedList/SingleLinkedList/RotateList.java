94b263c3851d508611c3e3d598419e13e200f91c
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-08-22 13:58:19 +0530

    Refactor rotation logic in linked list solutions

--- LinkedList/SingleLinkedList/RotateList.java
+++ LinkedList/SingleLinkedList/RotateList.java
@@ -8,7 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution { //Brute force
+class SolutionBrute { //Brute force
     private void reverse(int low,int high,List<Integer>list)
     {
         while(low<high)
@@ -44,3 +44,79 @@ class Solution { //Brute force
         return head;
     }
 }
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class SolutionOptimal { // Optimal 
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null||head.next==null||k==0)
+        return head;
+        ListNode tail = head;
+        ListNode slow = head;
+        ListNode fast = head;
+        int count =1;
+        while(tail.next!=null)
+        {
+            count++;
+            tail=tail.next;
+        }
+        tail.next=head;
+        k=k%count;
+        for(int i=1;i<=k;i++)
+        fast=fast.next;
+        while(fast!=tail)
+        {
+            slow=slow.next;
+            fast=fast.next;
+        }
+        head=slow.next;
+        slow.next=null;
+        return head;
+    }
+}
+//TIme complexity = o(n) + o(k) + o(n-k) = o(2n) = o(n) 
+//space complexity = o(1)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class SolutionOptimalV2 { //same as above optimal without fast pointer 
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null||head.next==null||k==0)
+        return head;
+        ListNode tail = head;
+        ListNode slow = head;
+        int count =1;
+        while(tail.next!=null)
+        {
+            count++;
+            tail=tail.next;
+        }
+        tail.next=head;
+        k=k%count;
+        for(int i=1;i<count-k;i++)
+        slow=slow.next;
+        
+        head=slow.next;
+        slow.next=null;
+        return head;
+    }
+}
+//TIme complexity = same as above o(2n) because o(n) + o(n-k) = o(2n) approx = o(n) 
+//Space complexity  = o(1)
