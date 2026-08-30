e56e134477e2d70cdd18f59234988f349a9eadb9
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-08-30 18:33:06 +0530

    Refactor invertTree method for clarity and simplicity
    Refactor invertTree method to swap left and right pointers directly without a separate handler function.

--- BinaryTree/InvertBInaryTree.java
+++ BinaryTree/InvertBInaryTree.java
@@ -29,3 +29,24 @@ class Solution {
         return root;
     }
 }
+//pattern swap left pointer and right pointer of each node
+//Time Complexity = o(2n+1) because every node call itself two timee in handler method and +1 from main so final = o(n) 
+//space complexity = same as above = o(n) 
+
+//Instead of creating handler function we can do this below one 
+
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null)
+        return root;
+        TreeNode temp = root.left;
+        root.left=root.right;
+        root.right=temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
+
+//Same time and space complexity 
