ee199aad2aaffd79bf474f71bc1b4a35e1c6d3b9
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-08-30 18:00:35 +0530

    Add postorder traversal method using stack
    Implemented postorder traversal using a stack and reversed the result list.

--- BinaryTree/PostOrder.java
+++ BinaryTree/PostOrder.java
@@ -29,3 +29,30 @@ class Solution { //using recursion
         return list;
     }
 }
+
+class Solution { //  using stack with root->right->left trick and then reverse
+    public List<Integer> postorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer>list = new ArrayList<>();
+        if(root!=null)
+        stack.push(root);
+        while(!stack.isEmpty())
+        {
+            root=stack.pop();
+            list.add(root.val);
+            if(root.left!=null)stack.push(root.left);
+            if(root.right!=null)stack.push(root.right);
+        }
+        int low=0;
+        int high = list.size()-1;
+        while(low<high)
+        {
+            int temp = list.get(low);
+            list.set(low,list.get(high));
+            list.set(high,temp);
+            low++;
+            high--;
+        }
+        return list;
+    }
+}
