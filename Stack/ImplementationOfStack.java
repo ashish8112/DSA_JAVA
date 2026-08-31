4ab4162ea237726de64b6ce156586770431ca68e
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-08-31 16:47:55 +0530

    Add ImplementationOfStack.java file

--- Stack/ImplementationOfStack.java
+++ Stack/ImplementationOfStack.java
@@ -64,6 +64,7 @@ class Main {
         stack.push(5);
         System.out.print(stack.top());
         System.out.println(stack.isEmpty());
+        System.out.println(stack);
     }
 }
 
@@ -104,7 +105,6 @@ class Stack<T>{ // Using Generic Stack in java
             System.out.println("Stack is empty");
             throw new java.util.EmptyStackException();
         }
-        System.out.println(stack[top]);
         return (T)stack[top];
     }
     boolean isEmpty(){
@@ -121,8 +121,87 @@ class Main {
         stack.push(4);
         stack.push(10);
         stack.push(5);
-        stack.top();
+        System.out.println(stack.top());
         System.out.println(stack.isEmpty());
     }
 }
 
+//Dynamic Stack
+
+class Stack<T>{ 
+    private int top = -1;
+    private Object[] stack; 
+    Stack(int size)
+    {
+        this.stack=new Object[size];
+    }
+
+    private void resize(){
+        System.out.println("Array full! Resizing length from "+stack.length+" to "+stack.length*2);
+        Object [] newStack = new Object[stack.length*2]; // creating brand new Object array of dobule size 
+        System.arraycopy(stack,0,newStack,0,stack.length); // copying all reference from old array to new array
+        this.stack=newStack; //swap old arraay refernce with new larger array
+    }
+    void push(T x)
+    {
+        if(top>=stack.length-1)
+            resize();
+        stack[++top]=x;
+        System.out.println("Pushed Element = "+stack[top]); 
+    }
+    @SuppressWarnings("unchecked")
+    T pop(){
+        if(top==-1)
+        {
+            System.out.println("Stack is underflow");
+            throw new java.util.EmptyStackException();
+        }
+        System.out.println("Popped Element = "+stack[top]); 
+        T element = (T) stack[top];
+        stack[top--]=null;
+        return element ;
+    }
+    @SuppressWarnings("unchecked")
+    T top(){
+        if(top==-1)
+        {
+            System.out.println("Stack is empty");
+            throw new java.util.EmptyStackException();
+        }
+        return (T)stack[top];
+    }
+    boolean isEmpty(){
+       return top==-1;
+    }
+
+    @Override
+    public String toString(){
+        if(top==-1)
+        return "[]";
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for(int i=0;i<=top;i++)
+        {
+            sb.append(stack[i]);
+            if(i<top)
+            sb.append(", ");
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Stack <Integer> stack = new Stack<>(5);
+        System.out.println(stack.isEmpty());
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(10);
+        stack.push(5);
+        stack.top();
+        System.out.println(stack.isEmpty());
+        System.out.println(stack);
+    }
+}
