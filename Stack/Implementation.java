8f125e729855dbfaecc6e381b19ee11e3f4e0311
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-08-31 16:34:17 +0530

    Implement toString method for Stack class
    Added a toString method to provide a string representation of the stack.

--- Stack/Implementation.java
+++ Stack/Implementation.java
@@ -35,6 +35,22 @@ class Stack{
     boolean isEmpty(){
        return top==-1;
     }
+
+        @Override 
+    public String toString(){
+        if(top==-1)
+            return "[]";
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for(int i=0;i<=top;i++)
+            {
+                sb.append(stack[i]);
+                if(i<top)
+                    sb.append(", ");
+            }
+        sb.append("]");
+        return sb.toString();
+    }
 }
 class Main {
     public static void main(String[] args) {
@@ -50,3 +66,63 @@ class Main {
         System.out.println(stack.isEmpty());
     }
 }
+
+class Stack<T>{ // Using Generic Stack in java
+    private int top = -1;
+    private Object[] stack; //Every index in Object stores reference or address of data 
+    Stack(int size)
+    {
+        this.stack=new Object[size];
+    }
+    void push(T x)
+    {
+        if(top>=stack.length-1)
+        {
+            System.out.println("Stack is overflow");
+            return ;
+        }
+        stack[++top]=x;
+        System.out.println("Pushed Element = "+stack[top]); // we are able to print value with reference because of object.toString() method which called by println under the hood
+    }
+    @SuppressWarnings("unchecked")
+    T pop(){
+        if(top==-1)
+        {
+            System.out.println("Stack is underflow");
+            throw new java.util.EmptyStackException();
+        }
+        System.out.println("Popped Element = "+stack[top]); // we are able to print value with reference because of object.toString() method which called by println under the hood
+        // return (T) stack[top--]; //Instead of this do below one because we are decreasing top and it is memory leak gc of java will not remove it 
+        T element = (T) stack[top];
+        stack[top--]=null;
+        return element ;
+    }
+    @SuppressWarnings("unchecked")
+    T top(){
+        if(top==-1)
+        {
+            System.out.println("Stack is empty");
+            throw new java.util.EmptyStackException();
+        }
+        System.out.println(stack[top]);
+        return (T)stack[top];
+    }
+    boolean isEmpty(){
+       return top==-1;
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Stack <Integer> stack = new Stack<>(5);
+        System.out.println(stack.isEmpty());
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(10);
+        stack.push(5);
+        stack.top();
+        System.out.println(stack.isEmpty());
+    }
+}
+
