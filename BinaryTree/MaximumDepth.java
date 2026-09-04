6774e5634369ee578c101534d7c0f78e5f7230e5
Ashish Kr. Shukla <122502973+ashish8112@users.noreply.github.com>
2026-09-04 17:20:00 +0530

    Add maxDepth method for calculating tree depth
    Implemented maxDepth method using level order traversal for optimal performance.

--- BinaryTree/MaximumDepth.java
+++ BinaryTree/MaximumDepth.java
@@ -13,6 +13,31 @@
  *     }
  * }
  */
+class Solution { //Optimal but using level order traversal
+    public int maxDepth(TreeNode root) {
+          if(root==null)
+          return 0;
+          Queue<TreeNode>q = new LinkedList<>();
+          q.add(root);
+          int depth =0;
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+            for(int i=0;i<size;i++)
+            {
+                TreeNode temp = q.remove();
+                if(temp.left!=null)q.add(temp.left);
+                if(temp.right!=null)q.add(temp.right);
+            }
+            depth++;
+        }
+        return depth;
+    }
+}
+
+//Time complexity = o(n) 
+//Space complexity = o(w) where w is maximum width  
+
 class Solution {
     public int maxDep(TreeNode root, int count){
         if(root==null)
